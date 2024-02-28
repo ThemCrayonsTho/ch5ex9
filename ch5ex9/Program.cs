f593afa5963fe85344ab9e5af317cc6e05bbb379
ThemCrayonsTho <bigmacchunk@outlook.com>
2024-02-28 13:22:19 -0600

    banan

--- ch5ex9/Program.cs
+++ ch5ex9/Program.cs
@@ -6,50 +6,60 @@ namespace ch5ex9
     {
         static void Main(string[] args)
         {
-            DisplayInstructions();
-            GetInput();
+            double pricePerLiter;
+            double pricePerGallon;
+            double convertedPricePerGallon;
 
+
+            DisplayInstructions();
+            pricePerLiter=GetInput("liters");
+            pricePerGallon = GetInput("gallons");
+            convertedPricePerGallon =ConvertLitersToGallons(pricePerLiter);
+            DisplayResults(pricePerLiter, pricePerGallon, convertedPricePerGallon);
+            Console.ReadKey();
         }
         public static void DisplayInstructions()
         {
-            Console.WriteLine("Itwhich gas station should be used?\n " +
+            Console.WriteLine("which gas station should be used?\n " +
             "The first value entered is price per liter at the Canadian station." +
-            "Inthe second value entered is the price per gallon at the American station." +
+            "\nthe second value entered is the price per gallon at the American station." +
             "\n\nA decision is made as to which station offers the most economical fuel price.");
-            Console.WriteLine("In\nPress any key when you are ready to begin...");
+            Console.WriteLine("\n\nPress any key when you are ready to begin...");
             Console.ReadKey();
             Console.Clear();
         }
         public static double GetInput(string whichone)
         {
             double price;
-            Console.Write("please enter fuel price per (0): ", whichone);
+            Console.Write("please enter fuel price per {0}: ", whichone);
             if (double.TryParse(Console.ReadLine(), out price) == false)
+            {
                 Console.Write("Hey, you didn't do it right...");
-            Console.ReadKey();
+                Console.ReadKey();
+            }
             return price;
         }
-        
+
         public static double ConvertLitersToGallons(double pricePerLiter)
         {
 
             return pricePerLiter * 3.7854;
         }
 
-        public static void DisplayResults(double priceperLiter, double pricePerGallon, double convertedPricePerGallon)
+        public static void DisplayResults(double pricePerLiter, double pricePerGallon, double convertedPricePerGallon)
         {
 
             Console.Clear();
-            Console.WriteLine("ItWhich station has the best fuel prices?");
-            Console.WriteLine("InAmerican Station - Price per gallon: (0:c)", pricePerGallon);
-            Console.WriteLine("Canadian station - Price per liter: (0:c)", priceperLiter);
-            Console.Write("Buy from the");
+            Console.WriteLine("\nWhich station has the best fuel prices?");
+            Console.WriteLine("\nAmerican Station - Price per gallon: {0:c}", pricePerGallon);
+            Console.WriteLine("Canadian station - Price per liter: {0:c}", pricePerLiter);
+            Console.Write("Buy from the ");
             if (convertedPricePerGallon > pricePerGallon)
                 Console.Write("American");
             else
                 Console.Write("Canadian");
             Console.WriteLine(" station!");
-            Console.WriteLine("ItCandaian liter converted price per gallon: (0:c)", convertedPricePerGallon);
+            Console.WriteLine("\nCandaian liter converted price per gallon: {0:c}", convertedPricePerGallon);
 
         }
 
